2b40c6fcf5e04cfeffbf9fbe3034230c5f8860c0
Amro Diab <adiab@hotmail.co.uk>
2017-10-14 18:11:58 +0100

    create binary file from main entry point

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
-#from distutils.core import setup
-from setuptools import setup
-from pip.req import parse_requirements
+#!/usr/bin/env python
 
-install_reqs = parse_requirements('requirements.txt',session='')
-reqs = [str(ir.req) for ir in install_reqs]
+"""
+Setup script for pdfgrep - a pdf grepping utility
+"""
+
+from setuptools import setup
+
+with open('requirements.txt', 'r') as reqs_file:
+    REQS = reqs_file.readlines()
+VER = '0.9'
 
 setup(
     name='pdfgrep',
     packages=['pdfgrep'],
-    version='0.8',
+    version=VER,
     description='a grep tool for pdf files',
     author='Amro Diab',
     author_email='adiab@linuxmail.org',
     url='https://github.com/adiabuk/pdfgrep',
-    download_url='https://github.com/adiabuk/pdfgrep/archive/0.8.tar.gz',
+    download_url=('https://github.com/adiabuk/pdfgrep/archive/{0}.tar.gz'
+                  .format(VER)),
     keywords=['pdf', 'grep'],
-    install_requires=reqs,
+    install_requires=REQS,
+    entry_points={'console_scripts':['pypdf=pdfgrep.pdfgrep:main']},
     classifiers=[],
 )
